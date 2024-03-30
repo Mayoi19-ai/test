1bb769142cdba032682704030fb9b89927b4586d
Mayoi19-ai <yukoakira1@gmail.com>
2024-03-29 18:24:32 -0600

    GGR290324 - Modificacion HelloWorld resta

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,10 +3,14 @@ package helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Suma: " + suma(1, 2));
+		System.out.println("Resta: " + resta(3, 2));
 	}
 
 	public static float suma(int a, int b) {
 		return a + b;
 	}
-
+	
+	public static float resta(int a, int b) {
+		return a - b;
+	}
 }
