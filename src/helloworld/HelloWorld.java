b32c7991a65ceb0d12a6faf24f43d8a5352da8d1
Mayoi19-ai <yukoakira1@gmail.com>
2024-03-29 18:13:15 -0600

    GGR290324 - Modificacion HelloWorld suma

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,11 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Suma: " + suma(1, 2));
 	}
+
+	public static float suma(int a, int b) {
+		return a + b;
+	}
+
 }
