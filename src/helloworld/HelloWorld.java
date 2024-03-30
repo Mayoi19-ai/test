36abb66ca54430178c2b376b1c28935a62e6966b
Mayoi19-ai <yukoakira1@gmail.com>
2024-03-29 19:16:39 -0600

    GGR290324 - Modificacion HelloWorld Persona

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Suma: " + suma(1, 2));
 		System.out.println("Resta: " + resta(3, 2));
+		Persona persona = new Persona();
+		persona.setApellido("any");
 	}
 
 	public static float suma(int a, int b) {
@@ -12,5 +14,9 @@ public class HelloWorld {
 	
 	public static float resta(int a, int b) {
 		return a - b;
+	}
+	
+	public static float restaInconveniente(int a, int b) {
+		return a - b + 2;
 	}
 }
